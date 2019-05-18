1a2e0e810b885e93cede43eab6b99b96452e08cc
Ashley Muller <serenehopes@gmail.com>
2019-05-18 03:21:15 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,3 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-<<<<<<< HEAD
-
-=======
->>>>>>> e11a2312362978c7b494f4d32c747ddb1b2e8b1b
 greeting = "Hello World"
